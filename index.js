b4f80c76e4b0589687f43ff9acf6c7f532657ba8
webdevagent <webdevagent@gmail.com>
2018-12-24 00:38:57 +0200

    Add code formatting

--- index.js
+++ index.js
@@ -9,55 +9,55 @@
 // Define your objects here
 
 
-  const Dog={
-    species: 'dog',
-    name:'Princess',
-    gender:'female',
-    paws:4,
-    voice:'woof',
-    friends:'Garry, Patrick, Wendy'
+const Dog = {
+  species: 'dog',
+  name: 'Princess',
+  gender: 'female',
+  paws: 4,
+  voice: 'woof',
+  friends: 'Garry, Patrick, Wendy'
 
-  };
-  const Cat={
-    species: 'cat',
-    name:'Garry',
-    gender:'male',
-    paws:4,
-    voice:'meow',
-    friends:'Princess, Patrick, Wendy, Kitty'
-  };
-  const Man={
-    species: 'human',
-    name:'Patrick',
-    gender:'male',
-    legs:2,
-    hands:2,
-    voice:'How do you do?',
-    friends:'Garry, Princess, Wendy, Kitty'
-  };
-  const Woman={
-    species: 'human',
-    name:'Wendy',
-    gender:'female',
-    legs:2,
-    hands:2,
-    voice:'Hello my friends',
-    friends:'Garry, Patrick, Princess'
-  };
-  const catWoman={
-    species: 'human',
-    name:'Kitty',
-    gender:'female',
-    paws:2,
-    hands:2,
-    voice:'meow',
-    friends:'Garry, Patrick'
-  };
-const residents=[Dog,Cat,Man,Woman];
+};
+const Cat = {
+  species: 'cat',
+  name: 'Garry',
+  gender: 'male',
+  paws: 4,
+  voice: 'meow',
+  friends: 'Princess, Patrick, Wendy, Kitty'
+};
+const Man = {
+  species: 'human',
+  name: 'Patrick',
+  gender: 'male',
+  legs: 2,
+  hands: 2,
+  voice: 'How do you do?',
+  friends: 'Garry, Princess, Wendy, Kitty'
+};
+const Woman = {
+  species: 'human',
+  name: 'Wendy',
+  gender: 'female',
+  legs: 2,
+  hands: 2,
+  voice: 'Hello my friends',
+  friends: 'Garry, Patrick, Princess'
+};
+const catWoman = {
+  species: 'human',
+  name: 'Kitty',
+  gender: 'female',
+  paws: 2,
+  hands: 2,
+  voice: 'meow',
+  friends: 'Garry, Patrick'
+};
+const residents = [Dog, Cat, Man, Woman];
 
-const showResidentInfo=()=>{
-  residents.forEach(num=>{
-    let residentData=Object.values(num);
+const showResidentInfo = () => {
+  residents.forEach(num => {
+    let residentData = Object.values(num);
     print(residentData.join('; '));
   });
 }
